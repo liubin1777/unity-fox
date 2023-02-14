b446e73a7d4d1551725d36db8c04c0123be7aa9e
robin <663907402@qq.com>
2023-02-15 07:25:40 +0800

    feat: 添加跳跃动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   public Rigidbody2D rb; // 刚体
-  public float speed = 10f; // 速度
+  public Animator animator; // 动画控制器
+  public LayerMask ground; // 地面图层
+  public Collider2D collider; // 碰撞体
+
+  public float speed = 400f; // 水平速度
+  public float jumpForce; // 跳跃的力
+
+
+  private bool isJump; // 是否跳跃状态
 
   // Start is called before the first frame update
   void Start()
@@ -13,23 +22,79 @@ public class PlayerController : MonoBehaviour
 
   }
 
-  // Update is called once per frame
+  // FixedUpdate 一般处理物理相关的
+  void FixedUpdate()
+  {
+    Movement();
+    SwitchAnim();
+  }
+
   void Update()
   {
+    checkInputStatus();
+  }
 
-    Movement();
+  // 检测输入系统
+  void checkInputStatus()
+  {
+    if (Input.GetButtonDown("Jump"))
+    {
+      this.isJump = true;
+    }
   }
 
   // 移动逻辑
   void Movement()
   {
-    // 获取水平输入
-    float horizontalMove = Input.GetAxis("Horizontal");
 
-    // -1: 向左，1：向右
+    // 获取水平输入
+    float horizontalMove = Input.GetAxis("Horizontal"); // 获取 -1。。。0.。。1 之间的小数
+    float faceDirection = Input.GetAxisRaw("Horizontal"); // 获取 -1 0 1 三个整数
+
+    // 水平速度x < 0: 向左，> 0：向右
     if (horizontalMove != 0)
     {
-      rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+      rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+      animator.SetFloat("running", MathF.Abs(horizontalMove));
+    }
+
+    // 水平方向
+    if (faceDirection != 0)
+    {
+      // Debug.Log("Input.GetButtonDown|水平方向");
+      transform.localScale = new Vector3(faceDirection, 1, 1);
+    }
+
+    // 跳跃
+    // if (Input.GetButtonDown("Jump"))
+    // if (Input.GetKeyDown(KeyCode.Space))
+    if (this.isJump)
+    {
+      // Debug.Log("Input.Jump = " + Input.GetAxis("Vertical"));
+      rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+      animator.SetBool("jumping", true);
+      this.isJump = false;
+    }
+  }
+
+  // 变换动画
+  void SwitchAnim()
+  {
+
+    animator.SetBool("idle", false);
+
+    if (animator.GetBool("jumping"))
+    {
+      if (rb.velocity.y < 0)
+      {
+        animator.SetBool("jumping", false);
+        animator.SetBool("falling", true);
+      }
+    }
+    else if (collider.IsTouchingLayers(ground))
+    {
+      animator.SetBool("falling", false);
+      animator.SetBool("idle", true);
     }
   }
 }
