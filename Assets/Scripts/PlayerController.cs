16f781116fad0671293c9e283b8b48ebea15ade4
robin <663907402@qq.com>
2023-02-20 07:34:54 +0800

    feat: 支持蹲下

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,19 @@ public class PlayerController : MonoBehaviour
   private Animator animator; // 动画控制器
 
   public LayerMask ground; // 地面图层
-  public Collider2D collider; // 碰撞体
+  public Collider2D circleCollider2D; // 碰撞体
+  public Collider2D boxCollider2D; // 碰撞体
 
   public float speed = 400f; // 水平速度
+  private float speedFactor; // 水平奔跑速度因子
+  private float faceDirection; // 水平朝向
+
   public float jumpForce; // 跳跃的力
 
   public int cherryCount; // 吃掉的樱桃个数
 
-
   private bool isJump; // 是否跳跃状态
+  private bool isCrouch; // 是否下蹲状态
 
   // Start is called before the first frame update
   void Start()
@@ -31,14 +35,15 @@ public class PlayerController : MonoBehaviour
   void FixedUpdate()
   {
     Movement();
-    SwitchAnim();
   }
 
   void Update()
   {
     checkInputStatus();
+    SwitchAnim();
   }
 
+  // 碰撞到触发器回调函数
   void OnTriggerEnter2D(Collider2D other)
   {
     if (other.tag == "Collection")
@@ -51,53 +56,75 @@ public class PlayerController : MonoBehaviour
   // 检测输入系统
   void checkInputStatus()
   {
+    // 获取水平输入
+    this.speedFactor = Input.GetAxis("Horizontal"); // 获取 -1。。。0.。。1 之间的小数
+    this.faceDirection = Input.GetAxisRaw("Horizontal"); // 获取 -1 0 1 三个整数
+
     // 检测跳跃
-    if (Input.GetButtonDown("Jump") && collider.IsTouchingLayers(ground))
+    if (Input.GetButtonDown("Jump") && circleCollider2D.IsTouchingLayers(ground))
     {
       this.isJump = true;
+    }
+    else if (!circleCollider2D.IsTouchingLayers(ground))
+    {
+      this.isJump = false;
+    }
+
+    // 检测下蹲
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+      this.isCrouch = true;
+      this.boxCollider2D.enabled = false;
+    }
+    else if (Input.GetKeyUp(KeyCode.S))
+    {
+      this.isCrouch = false;
+      this.boxCollider2D.enabled = true;
     }
   }
 
   // 移动逻辑
   void Movement()
   {
-
-    // 获取水平输入
-    float horizontalMove = Input.GetAxis("Horizontal"); // 获取 -1。。。0.。。1 之间的小数
-    float faceDirection = Input.GetAxisRaw("Horizontal"); // 获取 -1 0 1 三个整数
-
     // 水平速度x < 0: 向左，> 0：向右
-    if (horizontalMove != 0)
+    if (this.speedFactor != 0)
     {
-      rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
-      animator.SetFloat("running", MathF.Abs(horizontalMove));
+      rb.velocity = new Vector2(this.speedFactor * speed * Time.deltaTime, rb.velocity.y);
     }
 
     // 水平方向
-    if (faceDirection != 0)
+    if (this.faceDirection != 0)
     {
       // Debug.Log("Input.GetButtonDown|水平方向");
-      transform.localScale = new Vector3(faceDirection, 1, 1);
+      transform.localScale = new Vector3(this.faceDirection, 1, 1);
     }
 
     // 跳跃
-    // if (Input.GetButtonDown("Jump"))
-    // if (Input.GetKeyDown(KeyCode.Space))
     if (this.isJump)
     {
       // Debug.Log("Input.Jump = " + Input.GetAxis("Vertical"));
       rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
-      animator.SetBool("jumping", true);
-      this.isJump = false;
     }
+
   }
 
   // 变换动画
   void SwitchAnim()
   {
 
+    // 默认 idle false
     animator.SetBool("idle", false);
 
+    // 水平速度
+    animator.SetFloat("running", MathF.Abs(this.speedFactor));
+
+    // 跳跃
+    if (this.isJump)
+    {
+      animator.SetBool("jumping", true);
+    }
+
+    // 跳跃下落 or idle 切换
     if (animator.GetBool("jumping"))
     {
       if (rb.velocity.y < 0)
@@ -106,11 +133,14 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("falling", true);
       }
     }
-    else if (collider.IsTouchingLayers(ground))
+    else if (circleCollider2D.IsTouchingLayers(ground))
     {
       animator.SetBool("falling", false);
       animator.SetBool("idle", true);
     }
+
+    // 下蹲
+    animator.SetBool("crouch", this.isCrouch);
   }
 
 }
