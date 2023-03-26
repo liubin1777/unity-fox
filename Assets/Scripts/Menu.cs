6539efaac32429ecccfe9de0e1a08e5d2d8e255c
robin <663907402@qq.com>
2023-03-26 17:58:21 +0800

    feat: 暂停菜单AudioMixer

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+  public GameObject pauseMenu;
+  public AudioMixer audioMixer;
+
   public void playGame()
   {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,5 +22,22 @@ public class Menu : MonoBehaviour
   public void uiEnable()
   {
     GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
+  }
+
+  public void pauseGame()
+  {
+    pauseMenu.SetActive(true);
+    Time.timeScale = 0;
+  }
+
+  public void resumeGame()
+  {
+    pauseMenu.SetActive(false);
+    Time.timeScale = 1;
+  }
+
+  public void setVolume(float value)
+  {
+    audioMixer.SetFloat("MainVolume", value);
   }
 }
